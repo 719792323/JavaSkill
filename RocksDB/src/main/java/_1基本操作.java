e1e58a92a5df61ced98d0655962e6f4dc14fa9d2
SongJi <719792323@qq.com>
2023-04-14 11:17:43 +0800

    2023_4_14_1

---
+++ RocksDB/src/main/java/_1基本操作.java
@@ -0,0 +1,15 @@
+import org.rocksdb.RocksDB;
+
+public class _1基本操作 {
+    private final String DB_DIR = "data";
+    private RocksDB rocksDB;
+    //加载rocksdb
+    static {
+        RocksDB.loadLibrary();
+    }
+//    public RocksDBops
+
+    public static void main(String[] args) {
+
+    }
+}
